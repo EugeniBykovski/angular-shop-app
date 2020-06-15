4495f128a1f9a2879a66d53cd10807b3a264d7cc
Eugeni Bykovski <bykovskieugeni@mail.ru>
2020-06-15 17:33:57 +0300

    add spinner for Auth and add Auth service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthService } from "./auth.service";
+import { Observable } from "rxjs";
+
+import { AuthService, AuthResponseData } from "./auth.service";
 
 @Component({
   selector: 'app-auth',
@@ -8,6 +10,8 @@ import { AuthService } from "./auth.service";
 })
 export class AuthComponent {
   isLoginMode = true
+  isLoading = false
+  error: string = null
 
   constructor(private authService: AuthService) {}
 
@@ -23,19 +27,26 @@ export class AuthComponent {
     const email = form.value.email
     const password = form.value.password
 
+    let authObs: Observable<AuthResponseData>
+
+    this.isLoading = true // spinner
     if (this.isLoginMode) {
-      //...
+      authObs = this.authService.login(email, password)
     } else {
-      this.authService.signup(email, password)
-        .subscribe(
-          resData => {
-            console.log(resData);
-          }, 
-          error => {
-          console.log(error);
-          }
-        )
+      authObs = this.authService.signup(email, password)
       }
+
+      authObs.subscribe(
+        resData => {
+          console.log(resData);
+          this.isLoading = false
+        }, 
+        errorMessage => {
+          console.log(errorMessage);
+          this.error = errorMessage
+          this.isLoading = false
+        }
+      )
 
     form.reset()
   }
