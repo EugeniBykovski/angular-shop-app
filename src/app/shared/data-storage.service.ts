085013d2fd4b75c30c6c9ffc798bfa8219523a4a
Eugeni Bykovski <bykovskieugeni@mail.ru>
2020-06-15 18:56:10 +0300

    Attaching the Token with an Interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map, tap } from 'rxjs/operators';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { map, tap, take, exhaustMap } from 'rxjs/operators';
 
 import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(
+    private http: HttpClient, 
+    private recipeService: RecipeService,
+    private authService: AuthService  
+  ) {}
 
   storeRecipes() {
     const recipes = this.recipeService.getRecipes()
@@ -25,19 +30,24 @@ export class DataStorageService {
     return this.http
       .get<Recipe[]>(
         'https://ng-course-recipe-book-9683a.firebaseio.com/recipes.json'
-      )
-      .pipe(
-        map(recipes => {
-          return recipes.map(recipe => {
-            return {
-              ...recipe,
-              ingredients: recipe.ingredients ? recipe.ingredients : []
-            };
-          });
-        }),
-        tap(recipes => {
-          this.recipeService.setRecipes(recipes);
-        })
-      )
+      ).pipe(
+          map(
+            recipes => {
+              return recipes.map(
+                recipe => {
+                  return {
+                    ...recipe,
+                    ingredients: recipe.ingredients ? recipe.ingredients : []
+                  };
+                }
+              );
+            }
+          ),
+          tap(
+            recipes => {
+              this.recipeService.setRecipes(recipes);
+            }
+          )
+        )
   }
 }
