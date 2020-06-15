5e0f38f2eb0b697739d229e7b098ae004e21e39a
Eugeni Bykovski <bykovskieugeni@mail.ru>
2020-06-15 17:52:44 +0300

    add Logout button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false
+  private userSub: Subscription
+
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authService: AuthService
+  ) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.user
+      .subscribe(
+        user => {
+          this.isAuthenticated = !!user
+          console.log(!user);
+          console.log(!!user);
+        }
+      )
+  }
 
   onSaveData() {
     this.dataStorageService.storeRecipes()
@@ -14,5 +33,9 @@ export class HeaderComponent {
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe()
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe()
   }
 }
