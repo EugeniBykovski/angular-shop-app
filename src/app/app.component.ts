cb9079df72d051a22840f0a449329f202668fcaf
Eugeni Bykovski <bykovskieugeni@mail.ru>
2020-06-16 17:58:45 +0300

    Optimizing App and create Angular Modules

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth/auth.service';
+import { LoggingService } from './logging.service';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,12 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnInit {
   constructor(
-    private authService: AuthService
+    private authService: AuthService,
+    private loggingService: LoggingService
   ) {}
 
   ngOnInit() {
     this.authService.autoLogin()
+    this.loggingService.printLog('Hello from AppComponent ngOnInit')
   }
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -3,6 +3,7 @@ import { Subscription } from 'rxjs/Subscription';
 
 import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from './shopping-list.service';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-shopping-list',
@@ -13,7 +14,10 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   ingredients: Ingredient[];
   private subscription: Subscription;
 
-  constructor(private slService: ShoppingListService) { }
+  constructor(
+    private slService: ShoppingListService,
+    private loggingService: LoggingService
+  ) {}
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
@@ -23,6 +27,8 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
           this.ingredients = ingredients;
         }
       );
+
+    this.loggingService.printLog('Hello from ShoppingListComponent ngOnInit')
   }
 
   onEditItem(index: number) {
